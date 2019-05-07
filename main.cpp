12c14b7e39a2756cd1607af44c350d00e1cb037f
Liore Finkelstein <liorefinkel@gmail.com>
2019-05-07 17:37:09 +0300

    Add main

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <dlfcn.h>
+#include "AlgorithmRegistration.h"
+#include "Parser.h"
+#include "MatchManager.h"
+#include <functional>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    Parser p;
+    MatchManager& manager = MatchManager::getMatchManager();
+    manager.argMap = p.getMatchArgs(argc, argv);
+
+    manager.setup();
+    manager.run();
+
+
+    return 0;
+
+}
