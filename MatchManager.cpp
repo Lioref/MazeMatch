e4b8e6be5f2f57a65bcfa1ab97b29ba7fbb8dbd6
Liore Finkelstein <liorefinkel@gmail.com>
2019-05-08 14:52:43 +0300

    Fix the path concatenation for output files

--- MatchManager.cpp
+++ MatchManager.cpp
@@ -7,8 +7,6 @@
 
 #define DEBUG 0
 
-// TODO - take care of file paths with / and without /
-
 namespace fs = std::filesystem;
 
 MatchManager MatchManager::_singleton;
@@ -23,11 +21,12 @@ void MatchManager::run() {
             _resTable[algIt->first][mazeIt->first] = numSteps;
             if (fs::is_directory(outPath)) {
                 if (outPath != "")  {
-                    string fullOutPath = outPath + mazeIt->first + "_" + algIt->first + ".output";
+                    string fullOutPath = ((fs::path)outPath).append(mazeIt->first + "_" + algIt->first + ".output");;
                     gameManager.saveMoveLog(fullOutPath);
                 }
                 if (DEBUG) { // save logs for debug
-                    gameManager.savePositionLog(outPath + mazeIt->first + "_" + algIt->first + ".log");
+                    string logOutPath = ((fs::path)outPath).append(mazeIt->first + "_" + algIt->first + ".log");
+                    gameManager.savePositionLog(logOutPath);
                 }
             }
             if (DEBUG) {
@@ -101,7 +100,6 @@ void MatchManager::loadPuzzles() {
 void MatchManager::printSeperator(unsigned long len) {
     cout << string(len, '-') << endl;
 }
-
 
 void MatchManager::printResults() {
     // get list of algorithm names
